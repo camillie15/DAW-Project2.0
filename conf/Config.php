23c2cbc95cec8e4eec3461c7b3d66f0df0359cdf
ErickCor24 <erick.cordovavit@ug.edu.ec>
2025-01-15 23:12:10 -0500

    Create User module php & folders to config and more

---
+++ conf/Config.php
@@ -0,0 +1,19 @@
+<?php
+//controlador y funcion predefinida
+define("CONTROLADOR_PRINCIPAL", "index");
+define("FUNCION_PRINCIPAL", "index");
+
+
+//ruta de templates
+define("HEADER", 'view/templates/header.php');
+define("FOOTER", 'view/templates/footer.php');
+
+
+// path module view returns
+define("VRETURNS", "view/return/return.");
+
+
+// conexion bb
+define("DBNAME", "resolveit");
+define("DBUSER", "root");
+define("DBPASSWORD", "");
