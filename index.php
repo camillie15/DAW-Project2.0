9865888da32479905bd278f26d72473e0843386c
ErickCor24 <erick.cordovavit@ug.edu.ec>
2025-01-16 22:09:16 -0500

    fix bug of conifg & index folder

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 //FrontController
-require_once 'config/config.php';
+require_once 'conf/config.php';
 // leer parametros
 
 $controlador = (!empty($_REQUEST['c'])) ? htmlentities($_REQUEST['c']) : CONTROLADOR_PRINCIPAL;
