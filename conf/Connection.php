0719990e3d016a571931e80fa5c8aca522aea75d
Camillie Ayovi Villafuerte <camillieayovivillafuerte@gmail.com>
2025-01-16 20:09:57 -0500

    Config Connection.php

---
+++ conf/Connection.php
@@ -0,0 +1,15 @@
+<?php
+class Connection {
+    public static function getConnection() {
+        $dsn = 'mysql:host=localhost;port=3306;dbname=' . DBNAME;
+        $conexion = null;
+        try {
+            $conexion = new PDO($dsn, DBUSER, DBPASSWORD);
+             $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (Exception $e) {
+            echo $e;
+            die("Error " . $e->getMessage());
+        }      
+        return $conexion;
+    }
+}
