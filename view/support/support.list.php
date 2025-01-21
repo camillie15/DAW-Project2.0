db72bf0d8677a3a97f019fd3feb0edc8375238ee
Camillie Ayovi Villafuerte <camillieayovivillafuerte@gmail.com>
2025-01-21 00:33:29 -0500

    Update styles

--- view/support/support.list.php
+++ view/support/support.list.php
@@ -18,7 +18,7 @@ if (!isset($_SESSION)) {
         <?php endif; ?>
     </div>
     <div>
-        <table>
+        <table id="table-support">
             <thead>
                 <tr>
                     <?php if ($_SESSION['rol'] == 3): ?>
