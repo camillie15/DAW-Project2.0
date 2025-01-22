69a66e0a1d53c35d59d91b5a7a3e710d473c441f
DANIEL MATEO AGUILAR VILLAFUERTE <daniel.aguilarvil@ug.edu.ec>
2025-01-22 13:29:40 -0500

    Estilos User

--- view/user/user.update.php
+++ view/user/user.update.php
@@ -7,19 +7,19 @@
     <form action="index.php?c=user&f=processUpdate" method="POST" class="form-container">
         <div class="user-data">
             <label for="firstName" class="form-label">Nombre:</label>
-            <input type="text" name="firstName" value="<?php echo $_SESSION['userLogged']->getFirstName(); ?>" required><br>
+            <input type="text" name="firstName" class="form-input" value="<?php echo $_SESSION['userLogged']->getFirstName(); ?>" required><br>
 
             <label for="lastName" class="form-label">Apellido:</label>
-            <input type="text" name="lastName" value="<?php echo $_SESSION['userLogged']->getLastName(); ?>" required><br>
+            <input type="text" name="lastName" class="form-input" value="<?php echo $_SESSION['userLogged']->getLastName(); ?>" required><br>
 
             <label for="userName" class="form-label">Usuario:</label>
-            <input type="text" name="userName" value="<?php echo $_SESSION['userLogged']->getUserName(); ?>" required><br>
+            <input type="text" name="userName" class="form-input" value="<?php echo $_SESSION['userLogged']->getUserName(); ?>" required><br>
 
             <label for="email" class="form-label">Correo Electrónico:</label>
-            <input type="email" name="email" value="<?php echo $_SESSION['userLogged']->getEmail(); ?>" required><br>
+            <input type="email" name="email" class="form-input" value="<?php echo $_SESSION['userLogged']->getEmail(); ?>" required><br>
 
             <label for="password" class="form-label">Contraseña:</label>
-            <input type="password" name="password" value="<?php echo $_SESSION['userLogged']->getPassword(); ?>" required><br>
+            <input type="password" name="password" class="form-input" value="<?php echo $_SESSION['userLogged']->getPassword(); ?>" required><br>
         </div>
         
         <!-- Botón para actualizar -->
@@ -27,6 +27,11 @@
             <button type="submit" class="btn btn-primary">Actualizar</button>
         </div>
     </form>
+
+    <!-- Botón de eliminar (opcional) -->
+    <div class="actions">
+        <button type="button" class="btn btn-danger">Eliminar Cuenta</button>
+    </div>
 </main>
 
 <?php require_once FOOTER; ?>
