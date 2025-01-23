2ecfdd162347dd54af081b26e4ef3cef4360112f
JellySama22 <156031249+JellySama22@users.noreply.github.com>
2025-01-22 22:58:24 -0500

    Mateo/user (#22)
    * userModulo
    
    * Estilos User
    
    ---------
    
    Co-authored-by: DANIEL MATEO AGUILAR VILLAFUERTE <daniel.aguilarvil@ug.edu.ec>

--- view/user/user.login.php
+++ view/user/user.login.php
@@ -7,29 +7,24 @@
   <title>ResolveIT - Login</title>
   <link rel="stylesheet" href="assets/css/style.css">
   <link rel="stylesheet" href="assets/css/userStyle.css">
-
 </head>
 
 <body>
   <main class="main">
     <div class="profile-container">
       <h1>Login</h1>
-      <!-- Mostrar mensaje de error si las credenciales son incorrectas -->
       <?php if (isset($_GET['error']) && $_GET['error'] == '1'): ?>
-        <p class="error-msg" style="color: red;">Usuario o contraseña incorrectos.</p>
+        <p class="error-msg" style="color: red; font-weight: bold;">Usuario o contraseña incorrectos.</p>
       <?php endif; ?>
-      <div>
-      <form method="POST" action="index.php?c=user&f=processLogin">
-        <label for="userName">Username</label>
-        <input type="text" id="userName" name="userName" required>
+      <form method="POST" action="index.php?c=user&f=processLogin" class="form-container login-form">
+        <label for="userName" class="form-label">Username</label>
+        <input type="text" id="userName" name="userName" class="form-input" required>
 
-        <label for="password">Password</label>
-        <input type="password" id="password" name="password" required>
+        <label for="password" class="form-label">Password</label>
+        <input type="password" id="password" name="password" class="form-input" required>
 
         <button type="submit" class="btn btn-primary">Login</button>
       </form>
-      </div>
-      <!-- Mensaje para usuarios que no tienen cuenta -->
       <p>No tienes cuenta? <a href="index.php?c=user&f=register">Regístrate aquí</a></p>
     </div>
   </main>
