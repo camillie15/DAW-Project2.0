5b40ad9d69f2ff94b4f3b2b07d78703b409a0f6c
Cesar Tipan <cesar.tipanant@ug.edu.ec>
2025-01-23 19:21:17 -0500

    hola

--- controller/UserController.php
+++ controller/UserController.php
@@ -168,8 +168,8 @@ class UserController
         }
     }
     public function logout()
-    {
-        session_start();
+    {        
+        // session_start();
         session_unset();
         session_destroy();
         header('Location: index.php?c=user&f=login');
