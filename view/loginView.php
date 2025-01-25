477424f9f71ab0dae43fba35252309d63edb7851
Cesar Alexander Tipan Anton <101759547+CesarAlex87@users.noreply.github.com>
2025-01-24 21:41:22 -0500

    Feature/db cloud instance (#28)
    * Eliminar config.php del control de versiones y añadirlo a .gitignore
    
    * add: .gitignore
    
    * create: path files
    add: host and port to pdo connection
    
    * add: .gitignore
    
    * refactor: path
    
    * refactor: styles
    
    * fix: palabra
    
    * refactor: table name
    
    * add: vercel deploy related files
    
    * prueba2
    
    * fix: updated version
    
    * fix path
    
    * fix path
    
    * add DIR reference
    
    * fix dir reference
    
    * ya basta
    
    * path
    
    * fix paths
    
    * naming
    
    * TEMPORAL
    
    * temporal
    
    * fix name
    
    * remove git ignore
    
    * path fixes
    
    * fix paths
    
    * ????
    
    * el punto
    
    * test
    
    * Validaciones y eliminacion del body en faqStyle
    
    * vamoh a ver
    
    * ??
    
    * fds
    
    * fds
    
    * gfgd
    
    * hola
    
    * isset
    
    * remove destroy unset
    
    * sessions
    
    * if (session_status() == PHP_SESSION_NONE) {
        session_start();
    }
    
    * userLogged
    
    * verifylogin
    
    * fdsfds
    
    * add: dockerfile
    
    * Hasta aqui sirve // mas o menos
    
    * faq patch
    
    * add exit()
    
    * add: exit()
    
    * refactor: variable instead of function
    
    * refactor: return true instead of variable
    
    * use bind value instead
    
    * bindValue instead of bindParam
    
    * bindValue instead of param
    
    * fix paths for css and logo
    
    ---------
    
    Co-authored-by: Kiozzz <jimmypaez74@gmail.com>

--- view/loginView.php
+++ view/loginView.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>ResolveIT</title>
-    <link rel="stylesheet" href="/assets/css/style.css">
-    <link rel="stylesheet" href="/assets/css/userStyle.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/userStyle.css">
 </head>
 
 <main class="main">
@@ -14,7 +14,7 @@
     <div class="profile-container">
         <!-- Logo dentro del main -->
         <a class="logo-div" style="color: #000; display: flex; align-items: center; justify-content: center; gap: 10px;">
-            <img class="logo-img" src="/assets/img/logo.png" alt="Logo empresa" width="50" height="50">
+            <img class="logo-img" src="assets/img/logo.png" alt="Logo empresa" width="50" height="50">
             <h1 id="logo-name" style="margin: 0;">ResolveIT</h1>
         </a>
 
