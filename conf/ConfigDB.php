41a1184d611d8dc86a642baf372c3862ac5322b5
Cesar Alexander Tipan Anton <101759547+CesarAlex87@users.noreply.github.com>
2025-01-26 12:59:21 -0500

    add: local credentials (#31)

--- conf/ConfigDB.php
+++ conf/ConfigDB.php
@@ -1,7 +1,14 @@
 <?php
-// conexion bb
-define("DBNAME", "ProyectDAW_almostnot");
-define("DBUSER", "ProyectDAW_almostnot");
-define("DBPASSWORD", "8d15312e58412c89929c8f05f8910c04c727881e");
-define("DBPORT", "3307");
-define("DBHOST", "1f2f7.h.filess.io");  
+// Conexion a la base de datos en el servidor
+// define("DBNAME", "ProyectDAW_almostnot");
+// define("DBUSER", "ProyectDAW_almostnot");
+// define("DBPASSWORD", "8d15312e58412c89929c8f05f8910c04c727881e");
+// define("DBPORT", "3307");
+// define("DBHOST", "1f2f7.h.filess.io");  
+
+// Conexion a la base de datos en local
+define("DBNAME", "resolveit");
+define("DBUSER", "root");
+define("DBPASSWORD", "");
+define("DBPORT", "3306");
+define("DBHOST", "localhost");
